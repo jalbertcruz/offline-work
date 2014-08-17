a92f7c7c83498e392e782886169ebb265cbb16e9
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2014-08-17 15:45:27 +0200

    Cleaning for the version 2.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 offline-work
 ============
+
+
+wget http://hackage.haskell.org/packages/index.tar.gz
+
+wget GET http://hackage.haskell.org/api.json > api.txt
--- src/cloneHackage.py
+++ src/cloneHackage.py
@@ -1,74 +1,58 @@
-import urllib.request
+# import urllib.request
 import subprocess
 import os
 import sys
 
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
+import tarfile
 
 if len(sys.argv) > 1:
     os.chdir(sys.argv[1])
 
-def fetchUrl(url):
-    resp = urllib.request.urlopen(url)
-    data = resp.read()
-    t = data.decode('utf-8')
-    s = BeautifulSoup(t)
-    return t, s
+# def fetchUrl(url):
+# resp = urllib.request.urlopen(url)
+#     data = resp.read()
+#     t = data.decode('utf-8')
+#     s = BeautifulSoup(t)
+#     return t, s
 
-os.chdir('c:/Users/jalbert/Downloads/clojars')
-gHome = 0
-sHome = 0
-gAllPackages = 0
-sAllPackages = 0
-gAllPackagesVersions = 1
-sAllPackagesVersions = 0
+# os.chdir('c:/Users/jalbert/Downloads/clojars')
 
 prefix = 'http://hackage.haskell.org'
-if gHome:
-    text, soup = fetchUrl(prefix + "/packages/names")
-    if sHome:
-        open('data.html', 'w').write(text)
-        os._exit(0)
-else:
-    text = '\n'.join(open('data.html').readlines())
-    soup = BeautifulSoup(text)
 
-allPackages = []
-if gAllPackages:
-    for package in soup.find_all('ul')[1].find_all('li'):
-        allPackages.append(prefix + package.a['href'])
-    if sAllPackages:
-        open('allPackages.txt', 'w').write('\n'.join(allPackages))
-        os._exit(0)
-else:
-    allPackages = open('allPackages.txt').readlines()[:2]
-
-allPackagesVersions = []
-if gAllPackagesVersions:
-    for p1 in allPackages:
-        print(p1)
-        text, soup = fetchUrl(p1)
-        # for pversion in soup.find_all('table')[0]['tbody']['tr']['td']['a']:
-        for pversion in soup.find_all('table'):
-            # allPackagesVersions.append(pversion['href'])
-            print('Tabla')
-            for x in pversion.tr.td:
-                print(x)
-
-    if sAllPackagesVersions:
-        open('allPackagesVersions.txt', 'w').write('\n'.join(allPackagesVersions))
-        os._exit(0)
-else:
-    allPackagesVersions = open('allPackagesVersions.txt').readlines()[:20]
-
+subprocess.call(['wget', 'http://hackage.haskell.org/packages/index.tar.gz'])
 allPackagesUrls = []
-for p1 in allPackagesVersions:
-    text, soup = fetchUrl(p1)
-    for pversion in soup.find_all('a'):
-        href = str(pversion['href'])
-        if href.endswith('.tar.gz') or href.endswith('.cabal'):
-            allPackagesUrls.append(prefix + href)
+tf = tarfile.open('index.tar.gz')
+for m in tf.getmembers():
+    if str(m.name).endswith('.cabal'):
+        parts = m.name.split('/')
+        allPackagesUrls.append(
+            prefix + '/package/' + parts[0] + '-' + parts[1] + '/' + parts[0] + '-' + parts[1] + '.tar.gz')
+        # print(parts)
+# problems = []
+# good = []
+# for url in allPackagesUrls:
+#     try:
+#         resp = urllib.request.urlopen(url)
+#         good.append(url)
+#         # size += int(resp.headers['content-length'])
+#         # print(dict(resp.getheaders()))
+#     except Exception as e:
+#         print(e)
+#         problems.append(url)
 
 open('allPackagesUrls.txt', 'w').write('\n'.join(allPackagesUrls))
+# open('allPackagesUrls.txt', 'w').write('\n'.join(good))
+# print('With problems:', len(problems))
+# print(problems)
+# print('Ok:', len(good))
 
-# subprocess.call(['wget', '-i', 'allPackagesUrls.txt'])
+# try:
+# resp = urllib.request.urlopen('http://hackage.haskell.org/packages/index.tar.gz')
+# print('Tiene:', resp.headers['content-length'], 'bytes')
+# except Exception as e:
+#     print(e)
+os.mkdir('files')
+subprocess.call(['cp', 'allPackagesUrls.txt', 'files/allPackagesUrls.txt'])
+os.cddir('files')
+subprocess.call(['wget', '-i', 'allPackagesUrls.txt'])
