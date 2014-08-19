7972539812d2a25ca42f698d910be393f11b026b
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2014-08-19 12:48:42 +0200

    Refactoring.

--- src/hackageClone.py
+++ src/hackageClone.py
@@ -10,40 +10,57 @@ if len(sys.argv) > 1:
 
 prefix = 'http://hackage.haskell.org'
 
-subprocess.call(['wget', 'http://hackage.haskell.org/packages/index.tar.gz'])
-allPackagesUrls = []
-tf = tarfile.open('index.tar.gz')
-for m in tf.getmembers():
-    if str(m.name).endswith('.cabal'):
-        parts = m.name.split('/')
-        name = parts[0]
-        version = parts[1]
-        nameAndVersion = name + '-' + version
-        allPackagesUrls.append(prefix + '/package/' + nameAndVersion + '/' + nameAndVersion + '.tar.gz')
-        allPackagesUrls.append(prefix + '/package/' + nameAndVersion + '/' + name + '.cabal')
 
-        #http://hackage.haskell.org/package/3d-graphics-examples-0.0.0.0/3d-graphics-examples-0.0.0.0.tar.gz
-        #http://hackage.haskell.org/package/3d-graphics-examples-0.0.0.0/3d-graphics-examples.cabal
+def prepare():
+    subprocess.call(['wget', 'http://hackage.haskell.org/packages/index.tar.gz'])
+    allPackagesUrls = []
+    tf = tarfile.open('index.tar.gz')
+    for m in tf.getmembers():
+        if str(m.name).endswith('.cabal'):
+            parts = m.name.split('/')
+            name = parts[0]
+            version = parts[1]
+            nameAndVersion = name + '-' + version
+            allPackagesUrls.append(prefix + '/package/' + nameAndVersion + '/' + nameAndVersion + '.tar.gz')
+            # allPackagesUrls.append(prefix + '/package/' + nameAndVersion + '/' + name + '.cabal')
 
-open('allPackagesUrls.txt', 'w').write('\n'.join(allPackagesUrls))
+            # http://hackage.haskell.org/package/3d-graphics-examples-0.0.0.0/3d-graphics-examples-0.0.0.0.tar.gz
+            #http://hackage.haskell.org/package/3d-graphics-examples-0.0.0.0/3d-graphics-examples.cabal
 
-os.mkdir('files')
-subprocess.call(['mv', 'allPackagesUrls.txt', 'files'])
-os.chdir('files')
-subprocess.call(['wget', '-i', 'allPackagesUrls.txt'])
+    open('allPackagesUrls.txt', 'w').write('\n'.join(allPackagesUrls))
 
-os.chdir('..')
-os.mkdir('package')
-# subprocess.call(['7z', 'x', 'index.tar.gz'])
-# subprocess.call(['7z', 'x', '-y', '-o./index', 'index.tar'])
+    os.mkdir('files')
+    subprocess.call(['mv', 'allPackagesUrls.txt', 'files'])
 
-for m in tf.getmembers():
-    if str(m.name).endswith('.cabal'):
-        parts = m.name.split('/')
-        name = parts[0]
-        version = parts[1]
-        nameAndVersion = name + '-' + version
-        subprocess.call(['mkdir', './package/' + nameAndVersion])
-        subprocess.call(['mv', './files/' + nameAndVersion + '.tar.gz', './package/' + nameAndVersion])
 
-subprocess.call(['mv', 'index.tar.gz', 'package'])
+def download(compress=True):
+    os.chdir('files')
+    subprocess.call(['wget', '-i', 'allPackagesUrls.txt'])
+    if compress:
+        os.chdir('..')
+        tf = tarfile.open('files.tar.gz', 'w:gz')
+        tf.add('files')
+        tf.add('index.tar.gz')
+        tf.close()
+
+
+def organize():
+    os.mkdir('package')
+    # subprocess.call(['7z', 'x', 'index.tar.gz'])
+    # subprocess.call(['7z', 'x', '-y', '-o./index', 'index.tar'])
+    tf = tarfile.open('index.tar.gz')
+    for m in tf.getmembers():
+        if str(m.name).endswith('.cabal'):
+            parts = m.name.split('/')
+            name = parts[0]
+            version = parts[1]
+            nameAndVersion = name + '-' + version
+            subprocess.call(['mkdir', './package/' + nameAndVersion])
+            subprocess.call(['mv', './files/' + nameAndVersion + '.tar.gz', './package/' + nameAndVersion])
+            tf.extract(m, './package/' + nameAndVersion)
+            # subprocess.call(['mv', './files/' + name + '.cabal', './package/' + nameAndVersion])
+
+    subprocess.call(['mv', 'index.tar.gz', 'package'])
+
+prepare()
+download()
