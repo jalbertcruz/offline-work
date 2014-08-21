f201d2c4b24e33e9600d5a53dfab32fb664bb0cb
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2014-08-21 23:52:52 +0200

    Analizing dependencies

--- src/npmClone.py
+++ src/npmClone.py
@@ -16,14 +16,14 @@ def prepare():
     subprocess.call(['node', '../readRepos.js'])
     data = json.loads("".join(open("repos.json").readlines()))
     data = list(set(data))
-    open('repos.txt', "w").write('\n'.join(data))
+    open('repos.txt', "w").write('|'.join(data))
     names = [l[-1] for l in [e.split('/') for e in data]]
     duplicados = [x for x, y in collections.Counter(names).items() if y > 1]
     open('duplicados.txt', "w").write('\n'.join(duplicados))
 
 
 def cloneRepos(n, m):
-    data = open('repos.txt').readlines()
+    data = open('repos.txt').readline().split('|')
     for d in data[n:m]:
         if d[-1] == '\n':
             d = d[:-1]
@@ -46,9 +46,11 @@ def cloneRepos(n, m):
         subprocess.call(['rm', '-R', repo])
         os.chdir('..')
 
+
 def cloneAllRepos():
-    data = open('repos.txt').readlines()
+    data = open('repos.txt').readline().split('|')
     cloneRepos(0, len(data))
+
 
 # cloneRepos(0, 3)
 def downloadAll():
@@ -60,4 +62,47 @@ def downloadAll():
     tf.close()
     subprocess.call(['rm', '-R', 'npm'])
 
-downloadAll()
+
+def checkDependencies():
+    data = open('repos.txt').readline().split('|')
+    allRepos = set()
+    for d in data:
+        e = d.split('/')
+        user = e[-2]
+        repo = e[-1]
+        subprocess.call(['mkdir', user])
+        os.chdir(user)
+        subprocess.call(['mkdir', repo])
+        os.chdir(repo)
+        subprocess.call(
+            ['wget', '--no-check-certificate', 'https://raw.github.com/' + user + '/' + repo +'/master/package.json'])
+        if os.path.isfile("package.json"):
+            pk = json.loads("".join(open("package.json").readlines()))
+            if 'name' in pk:
+                allRepos.add(pk['name'])
+        os.chdir('..')
+        os.chdir('..')
+    missing = set()
+    for d in data:
+        e = d.split('/')
+        user = e[-2]
+        repo = e[-1]
+        os.chdir(user)
+        os.chdir(repo)
+        if os.path.isfile("package.json"):
+            pk = json.loads("".join(open("package.json").readlines()))
+            deps = set()
+            if 'dependencies' in pk:
+                for k in pk['dependencies']:
+                    deps.add(k)
+            if 'devDependencies' in pk:
+                for k in pk['devDependencies']:
+                    deps.add(k)
+            missing.update(deps - allRepos)
+        os.chdir('..')
+        os.chdir('..')
+    open('missing.txt', 'w').write('\n'.join(missing))
+
+# downloadAll()
+# prepare()
+checkDependencies()
